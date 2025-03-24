807e3d58c14e7fa0ce954154756657d0c65b2ae2
Harnish <harrnish@gmail.com>
2025-03-24 13:50:55 -0400

    fonts and footer

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+import Footer from "../../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div className="page home">
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
