74c894a44463fdb3e78ddf6d3d54c9d894dc6926
Harnish <harrnish@gmail.com>
2025-03-24 22:44:27 -0400

    work carousel structure

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -4,7 +4,37 @@ import "./Work.css";
 const Work = () => {
   return (
     <div className="page work">
-      <h1>Work</h1>
+      <div className="work-carousel">
+        <div className="work-slider-img">
+          <img src="/work/work-1.jpg" alt="" />
+        </div>
+
+        <div className="work-items-preview-container">
+          <div className="work-item active">
+            <img src="/work/work-1.jpg" alt="" />
+          </div>
+          <div className="work-item">
+            <img src="/work/work-2.jpg" alt="" />
+          </div>
+          <div className="work-item">
+            <img src="/work/work-3.jpg" alt="" />
+          </div>
+          <div className="work-item">
+            <img src="/work/work-4.jpg" alt="" />
+          </div>
+          <div className="work-item">
+            <img src="/work/work-5.jpg" alt="" />
+          </div>
+          <div className="work-item">
+            <img src="/work/work-6.jpg" alt="" />
+          </div>
+        </div>
+
+        <div className="carousel-info">
+          <p className="primary sm">Project Desc 1</p>
+          <h1>Project Title 1</h1>
+        </div>
+      </div>
     </div>
   );
 };
